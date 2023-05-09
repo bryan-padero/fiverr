1b5c25bcb3bef03c4dc602d16422b74e74866dbe
Bryan <bryanpadero@gmail.com>
2023-05-09 21:45:55 +0400

    added Gig CRUD

--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,10 +1,24 @@
 // imports
 import express from 'express'
 
+// controllers 
+
+import { 
+    createGig, 
+    deleteGig, 
+    getGig, 
+    getGigs } from '../controllers/gig.controller.js'
+
+// middlewares
+import { verifyToken } from '../middlewares/jwt.js'
+
 // controller imports
 
 const router = express.Router()
 
-router.get("/test", )
+router.post("/", verifyToken, createGig)
+router.delete("/:id", verifyToken, deleteGig)
+router.get("/single/:id", verifyToken, getGig)
+router.get("/", verifyToken, getGigs)
 
 export default router
